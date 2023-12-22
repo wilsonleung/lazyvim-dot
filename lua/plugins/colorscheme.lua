fcf0c78363da84baea25c5ad69a1a255253e3084
Wilson Leung <wilsonpsleung@gmail.com>
2023-12-22 22:20:32 +0800

    add active window highlight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,32 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = function()
+      return {
+        flavour = "frappe",
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
+        color_overrides = {},
+        dim_inactive = {
+          enabled = true,
+          shade = "dark",
+          percentage = 0.01,
+        },
+      }
+    end,
+  },
   -- add gruvbox
-  -- { "ellisonleao/gruvbox.nvim" },
-
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
