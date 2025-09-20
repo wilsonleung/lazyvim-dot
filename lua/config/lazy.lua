a239f5afb78750d382748b590d7b5164c2a1f578
Wilson Leung <wilsonleung@Wilsons-MacBook-Air.local>
2025-09-20 15:44:01 +0800

    add typescript support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -50,4 +50,7 @@ require("lazy").setup({
       },
     },
   },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
 })
