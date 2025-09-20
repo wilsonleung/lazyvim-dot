a4577578a4d2e69f5c40033049baeb7c066c73ce
Wilson Leung <wilsonleung@Wilsons-MacBook-Air.local>
2025-09-20 22:34:59 +0800

    update the explorer setup

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,6 +3,32 @@ return {
   opts = {
     picker = {
       enabled = true,
+      sources = {
+        explorer = {
+          preview = false,
+          layout = {
+            preview = "main",
+            layout = {
+              backdrop = false,
+              width = 40,
+              min_width = 40,
+              height = 0,
+              position = "left",
+              border = "none",
+              box = "vertical",
+              {
+                win = "input",
+                height = 1,
+                border = "rounded",
+                title = "{title} {live} {flags}",
+                title_pos = "center",
+              },
+              { win = "list", border = "none" },
+              { win = "preview", title = "{preview}", height = 0.4, border = "top" },
+            },
+          },
+        },
+      },
       layout = {
         layout = {
           backdrop = false,
