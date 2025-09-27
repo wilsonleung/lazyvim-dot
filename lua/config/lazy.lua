3e7073ae265915595c3f25c586c6516bdef93df2
Wilson Leung <wilsonleung@Wilsons-MacBook-Air.local>
2025-09-27 16:00:53 +0800

    update the lazyextras plugins settings

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,10 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -50,7 +54,4 @@ require("lazy").setup({
       },
     },
   },
-  { import = "lazyvim.plugins.extras.formatting.prettier" },
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.linting.eslint" },
 })
